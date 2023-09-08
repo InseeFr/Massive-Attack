160bb13383c0ef2a44d6755d052afe8b45009c11
MickaelMenet <mickaelmenet@gmail.com>
2023-09-08 14:56:23 +0200

    Create csvUploader : issue => Pouvoir charger une liste d'idep

---
+++ src/components/requester/CsvUploader.js
@@ -0,0 +1,52 @@
+import Papa from 'papaparse';
+
+export const handleCSVUpload = async (event, setInterviewers, setInvalidValues) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = async e => {
+    const contents = e.target.result;
+    const newInvalidValues = []; // Use a temporary array to store invalid values
+
+    // Use Papaparse to parse the CSV file
+    Papa.parse(contents, {
+      delimiter: ',', // Set the appropriate delimiter
+      header: true, // Specify if the first row is a header (containing column names)
+      dynamicTyping: true, // Attempt to automatically detect data types
+      complete: result => {
+        // 'result.data' contains the parsed data from the CSV
+        const allValues = [];
+
+        // Iterate through each row of the CSV
+        result.data.forEach(row => {
+          // Iterate through each column of the row
+          Object.keys(row).forEach(columnName => {
+            const value = row[columnName];
+            if (
+              value !== null &&
+              typeof value === 'string' &&
+              value.trim().length === 6 &&
+              value.trim().toUpperCase() === value.trim()
+            ) {
+              allValues.push(value.trim().toUpperCase());
+            } else {
+              if (value !== null) {
+                newInvalidValues.push(value); // Store invalid values
+              }
+            }
+          });
+        });
+
+        // Update the 'interviewers' state with valid values
+        setInterviewers(allValues.map((value, index) => ({ id: value, index })));
+        // Update the 'invalidValues' state with invalid values
+        setInvalidValues(newInvalidValues);
+      },
+      error: error => {
+        console.error('Error parsing the CSV file:', error.message);
+      },
+    });
+  };
+
+  reader.readAsText(file);
+};
