2f424a99e47f139d2c0fe01c6fa7750abb4094a0
MickaelMenet <mickaelmenet@gmail.com>
2023-09-20 08:40:33 +0200

    fix for unique values

--- src/components/requester/CsvUploader.js
+++ src/components/requester/CsvUploader.js
@@ -1,42 +1,39 @@
 import Papa from 'papaparse';
+
 export const handleCSVUpload = async (event, setInterviewers, setInvalidValues, showAlert) => {
   const file = event.target.files[0];
   const reader = new FileReader();
 
   reader.onload = async e => {
     const contents = e.target.result;
-    const newInvalidValues = []; // Use a temporary array to store invalid values
+    const newInvalidValues = [];
+    const uniqueValues = new Set();
 
-    // Use Papaparse to parse the CSV file
     Papa.parse(contents, {
-      delimiter: ',', // Set the appropriate delimiter
-      header: true, // Specify if the first row is a header (containing column names)
-      dynamicTyping: true, // Attempt to automatically detect data types
+      delimiter: ',',
+      header: true,
+      dynamicTyping: true,
       complete: result => {
-        // 'result.data' contains the parsed data from the CSV
-        const allValues = [];
-
-        // Iterate through each row of the CSV
         result.data.forEach(row => {
-          // Iterate through each column of the row
           Object.keys(row).forEach(columnName => {
             const value = row[columnName];
             if (value !== null && typeof value === 'string' && value.trim().length === 6) {
-              allValues.push(value.trim().toUpperCase());
+              const uppercasedValue = value.trim().toUpperCase();
+              if (!uniqueValues.has(uppercasedValue)) {
+                uniqueValues.add(uppercasedValue);
+              }
             } else {
               if (value !== null) {
-                newInvalidValues.push(value); // Store invalid values
+                newInvalidValues.push(value);
               }
             }
           });
         });
 
-        // Update the 'interviewers' state with valid values
-        setInterviewers(allValues.map((value, index) => ({ id: value, index })));
-        // Update the 'invalidValues' state with invalid values
+        const uniqueValuesArray = Array.from(uniqueValues);
+        setInterviewers(uniqueValuesArray.map((value, index) => ({ id: value, index })));
         setInvalidValues(newInvalidValues);
 
-        // Show the alert with the message including invalidValues
         if (newInvalidValues.length > 0) {
           showAlert(
             `The following elements were not considered (expected: 6 uppercase characters): ${newInvalidValues.join(
@@ -47,7 +44,7 @@ export const handleCSVUpload = async (event, setInterviewers, setInvalidValues, 
         }
       },
       error: error => {
-        showAlert(`Erreur lors de l'analyse du fichier CSV : ${error.message}`, 'error');
+        showAlert(`Error analyzing CSV file : ${error.message}`, 'error');
         console.error('Error parsing the CSV file:', error.message);
       },
     });
