9e231480197458c84504fcbc76f4415765cbe9eb
Simon Demaziere <simon.demaziere@insee.fr>
2024-11-27 17:44:01 +0100

    fix: bump react-script

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core';
-import { version } from '../../../package.json';
+import packagejson from '../../../package.json';
 import { LOC_STOR_PLATFORM_KEY } from 'utils/constants';
 
+const appVersion = packagejson.version;
 const useStyles = makeStyles(() => ({
   row: {
     display: 'flex',
@@ -33,7 +34,7 @@ const Header = ({ user = { firstName: 'John', lastName: 'Doe' } }) => {
       <div className={classes.textBlock}>
         <Typography variant="h4">MASSIVE ATTACK</Typography>
         <Typography className={classes.marginLeft} variant="h6" color="error">
-          {version}
+          {appVersion}
         </Typography>
       </div>
       <Typography variant="h4">{`==> ${pf}`}</Typography>
